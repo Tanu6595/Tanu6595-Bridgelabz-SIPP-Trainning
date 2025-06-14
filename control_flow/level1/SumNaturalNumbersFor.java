a94a4d67c87c03f5ffe2ee7eb70a280846168810
Tanu6595 <tanu.y80063@gmail.com>
2025-06-14 13:44:41 +0530

    Added control_flow/src folder to Core-Java branch

---
+++ control_flow/level1/SumNaturalNumbersFor.java
@@ -0,0 +1,28 @@
+package level1;
+import java.util.Scanner;
+
+public class SumNaturalNumbersFor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a natural number: ");
+        int n = scanner.nextInt();
+        
+        if (n < 1) {
+            System.out.println("The number " + n + " is not a natural number.");
+        } else {
+            int sumFormula = n * (n + 1) / 2;
+            int sumFor = 0;
+
+            for (int i = 1; i <= n; i++) {
+                sumFor += i;
+            }
+
+            System.out.println("Sum using formula: " + sumFormula);
+            System.out.println("Sum using for loop: " + sumFor);
+            System.out.println("Are both results equal? " + (sumFormula == sumFor));
+        }
+ 
+    }
+}
+ 
+ 

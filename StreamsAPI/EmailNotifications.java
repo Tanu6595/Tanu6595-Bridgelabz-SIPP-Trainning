762eda51ee18aa873c256e6df79fb583318bec1b
Tanu6595 <tanu.y80063@gmail.com>
2025-09-15 08:35:46 +0530

    Remove folder from repo

--- StreamsAPI/EmailNotifications.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class EmailNotifications {
-    public static void main(String[] args) {
-        List<String> emails = Arrays.asList("alice@example.com", "bob@example.com", "charlie@example.com");
-
-        emails.forEach(email -> System.out.println("Sending notification to: " + email));
-    }
-}
